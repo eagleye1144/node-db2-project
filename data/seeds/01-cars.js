04279212bd9bde78b33f66d2e02bb7aab97bdebf
Mitchell Walker <eagleye1144@gmail.com>
2021-11-02 15:54:46 -0600

    done

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,39 @@
-// STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '22222222222222',
+        make: 'toyota',
+        model: 'corrolla',
+        mileage: 115000,
+        title: 'salvage',
+
+    },
+    {
+        vin: '3333333333333',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000,
+        title: 'salvage',
+
+    }
+
+]
+
+
+
+
+
+
+
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
